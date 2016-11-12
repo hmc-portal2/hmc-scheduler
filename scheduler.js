40cace2e43d9a508038a5853b1983d4d72a414e7
g_rocket <g.rocket1@gmail.com>
2016-11-12 04:05:13 -0800

    pull in some filtering

--- scheduler.js
+++ scheduler.js
@@ -1,5 +1,16 @@
+var globalCourseData = [];
+var globalCourseSearch = [];
+
 function lingkCallback(json) {
-  
+  globalCourseData = json['data'];
+  updateSearch();
+}
+
+function updateSearch() {
+  globalCourseSearch = globalCourseData;
+  globalCourseSearch = filterCoursesByCalendar(globalCourseSearch, "designator", "SP2017");
+  globalCourseSearch = getCourseFromAttributeRegex(globalCourseSearch, "courseNumber", /.*070.*/);
+  for(var data of globalCourseSearch) console.log(toCourseObject(data).times);
 }
 
 function toAmPmTime(timestring) {
@@ -23,7 +34,6 @@ function toCourseObject(courseJson) {
   for(var section of courseJson['courseSections']) {
     if(!isfirsttimeslot) {
       timeslots += '\n';
-      isfirsttimeslot = true;
     }
     var instructorName = '';
     if(section['sectionInstructor'] && section['sectionInstructor'].length > 0) {
@@ -58,6 +68,7 @@ function toCourseObject(courseJson) {
       timeslot += schedule['instructionSiteName'];
     }
     timeslots += timeslot;
+    isfirsttimeslot = false;
   }
   return {
     name: courseName,
@@ -748,6 +759,85 @@ function messageOnce(str) {
 }());
 
 
+///////////////////// START PARSER /////////////////////////////////////
+
+function getCourseSections(course) {
+    return course["courseSections"];
+}
+
+function filterCoursesByCalendar(courses, calendarAttribute, expected) {
+    var filteredCourses = [];
+    for(course of courses) {
+        var correctedCourse = JSON.parse(JSON.stringify(course));
+        var sectionsForCourse = filterSectionsByCalendar(getCourseSections(course),
+                calendarAttribute, expected);
+        correctedCourse["courseSections"] = sectionsForCourse; 
+        if(sectionsForCourse.length != 0) {
+            filteredCourses.push(correctedCourse);
+        }
+    }
+    return filteredCourses;
+}
+
+function filterSectionsByCalendar(sections, attribute, expected) {
+    var filteredSections = []; // Has invalid sections removed.
+    for(section of sections) {
+        var correctedSection = JSON.parse(JSON.stringify(section)); // Has invalid calendar sessions removed.
+        var filteredCalendarSessions = []; // Valid calendar session array.
+        for(calendarSession of section["calendarSessions"]) {
+            if(calendarSession[attribute] === expected) {
+                filteredCalendarSessions.push(calendarSession);
+            }
+        }
+        correctedSection["calendarSessions"] = filteredCalendarSessions;
+        // If there are no calendar sections left for this section,
+        // remove the section.
+        if(filteredCalendarSessions.length != 0) {
+            filteredSections.push(correctedSection);
+        }
+    }
+    return filteredSections;
+}
+
+function getCoursesFromAttribute(response, attribute, expected) {
+    var possibleCourses = [];
+    for(key of response) {
+        if(key[attribute] === expected) {
+            possibleCourses.push(key);
+        }
+    }
+    return possibleCourses;
+}
+
+function getCourseFromAttributeRegex(response, attribute, expression) {
+    var possibleCourses = [];
+    for(key of response) {
+        if(key[attribute]) {
+            //console.log(key[attribute]);
+            if(key[attribute].match(expression)) {
+                possibleCourses.push(key);
+            }
+        }
+    }
+    return possibleCourses;
+}
+
+function attributeFilter(response, attribute, expected, mustBe) {
+    var filtered = [];
+    for(key in response) {
+        // Push the response object to the filtered array only if
+        // if has the correct attribute when we want a specific attribute,
+        // or if it does not have a specific attribute.
+        if((response[key][attribute] === expected && mustBe)
+            || (response[key][attribute] != expected && !mustBe)) {
+            filtered.push(response[key]);
+        }
+    }
+    return filtered;
+}
+
+
+////////////////////////// END PARSER ////////////////////////////////////
 
 
 
